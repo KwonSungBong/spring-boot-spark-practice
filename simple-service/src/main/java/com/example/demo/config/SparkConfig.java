13623f6accf6ba4352962775323ce5e72d68a512
ksb <rnjstjdqhd3939@gmail.com>
2018-05-19 08:59:05 +0900

    test

---
+++ simple-service/src/main/java/com/example/demo/config/SparkConfig.java
@@ -0,0 +1,48 @@
+package com.example.demo.config;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class SparkConfig {
+
+    @Autowired
+    private Environment env;
+
+    private String appName = "test";
+
+    private String sparkHome = "../spark/spark-2.2.0-bin-hadoop2.7";
+
+    private String masterUri = "local";
+
+    @Bean
+    public SparkConf sparkConf() {
+        SparkConf sparkConf = new SparkConf()
+                .setAppName(appName)
+                .setSparkHome(sparkHome)
+                .setMaster(masterUri)
+                .set("spark.driver.host", "localhost");
+
+        return sparkConf;
+    }
+
+    @Bean
+    public JavaSparkContext javaSparkContext() {
+        return new JavaSparkContext(sparkConf());
+    }
+
+    @Bean
+    public SparkSession sparkSession() {
+        return SparkSession
+                .builder()
+                .sparkContext(javaSparkContext().sc())
+                .appName("Java Spark SQL basic example")
+                .getOrCreate();
+    }
+
+}
--- simple-service/src/test/java/com/example/demo/service/SparkServiceTests.java
+++ simple-service/src/test/java/com/example/demo/service/SparkServiceTests.java
@@ -1,15 +1,14 @@
 package com.example.demo.service;
 
-import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.SparkSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * Created by ksb on 2018. 2. 5..
@@ -19,40 +18,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SparkServiceTests {
 
     @Autowired
-    private Environment env;
-
-    private String appName = "test";
-
-    private String sparkHome = "/home/whilemouse/git/spark/spark-2.2.0-bin-hadoop2.7";
-
-    private String masterUri = "local";
-
-    @Bean
-    public SparkConf sparkConf() {
-        SparkConf sparkConf = new SparkConf()
-                .setAppName(appName)
-                .setSparkHome(sparkHome)
-                .setMaster(masterUri);
-
-        return sparkConf;
-    }
-
-    @Bean
-    public JavaSparkContext javaSparkContext() {
-        return new JavaSparkContext(sparkConf());
-    }
-
-    @Bean
-    public SparkSession sparkSession() {
-        return SparkSession
-                .builder()
-                .sparkContext(javaSparkContext().sc())
-                .appName("Java Spark SQL basic example")
-                .getOrCreate();
-    }
+    private JavaSparkContext sc;
 
     @Test
     public void test() {
+        JavaRDD<String> inFile = sc.textFile("../spark/csv/simple.csv");
+
+        List<String> stringList = inFile.collect();
+
+        System.out.println();
 
     }
 
